43b115d80b8b825630aa8457b1b04c0d29f2d222
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-04-06 12:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const makeChange = (c) => {
   // your name here
+	if(c>0 && c<100) {
+		const change = {};
+
+    change.q = Math.floor(c / 25);
+    c = c % 25;
+
+    change.d = Math.floor(c / 10);
+    c = c % 10;
+
+    change.n = Math.floor(c / 5);
+    c = c % 5;
+
+    change.p = c;
+
+    return change;
+	}
+	return { q: 0, d: 0, n: 0, p: 0 };
 };
 
 // Do not the change the code below
